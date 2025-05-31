b6d831a65ecf17376ec9a583d75d3368a65164cf
Yarima <151117728+YarimaGroup@users.noreply.github.com>
2025-05-31 11:35:29 +0530

    Add scroll to top behavior

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useScrollToTop } from '../../hooks/useScrollToTop';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  useScrollToTop();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
